b734a6a5cc73b14a364c1334b7814427a8266cf8
Overthrive <sara.leal.lessa@gmail.com>
2022-03-09 00:55:11 -0300

    MovieCard

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,9 +1,29 @@
 import Pagination from "components/pagination";
+import MovieCard from "components/MovieCard";
+
 import './style.css';
 
 function Listing(){
     return(
-        <Pagination/>
+        <>
+        <Pagination />
+        <div className="container">
+        <div className="row">
+            <div className="col-md-6">
+                <MovieCard />
+            </div>    
+            <div className="col-md-6">
+                <MovieCard />
+            </div>    
+            <div className="col-md-6">
+                <MovieCard />
+            </div>    
+            <div className="col-md-6">
+                <MovieCard />
+            </div>    
+        </div>
+        </div>
+        </>
     );
 }
 
