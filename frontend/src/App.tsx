8d58f8b5313fcfda6f29ebcabe792761f4c2c9db
Overthrive <sara.leal.lessa@gmail.com>
2022-03-09 00:02:23 -0300

    Routas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import NavBar from "./components/navbar/index";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/listing';
+import Form from 'pages/form';
+import Navbar from "components/navbar/index";
 
 function App() {
   return (
-    <NavBar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
