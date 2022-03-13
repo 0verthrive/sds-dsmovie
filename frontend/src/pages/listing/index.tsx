eee1b614a8b313f200db67e96fa97741c846aed3
Overthrive <sara.leal.lessa@gmail.com>
2022-03-13 10:07:03 -0300

    useState, useEffect

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,19 +1,26 @@
 import Pagination from "components/pagination";
 import MovieCard from "components/MovieCard";
 import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie";
 
+import { useEffect, useState } from "react";
 import './style.css';
 import axios from "axios";
 
 function Listing(){
-
-    //Errado:
-    axios.get(`${BASE_URL}/movies?size=12&page=0`).then(response => {
-        console.log(response.data);
-    });
-
+    //Mantem o estado no componente
+    const [pageNumber, setPageNumber] = useState(0);
+    //Executar algo na instanciação ou destruição do componente, observar estado
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=0`).then(response => {
+            const data = response.data as MoviePage;
+            setPageNumber(data.number);
+        });
+    }, [])
+    
     return(
         <>
+        <p>{pageNumber}</p>
         <Pagination />
         <div className="container">
         <div className="row">
