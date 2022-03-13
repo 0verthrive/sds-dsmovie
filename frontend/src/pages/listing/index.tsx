d2a7762b212ce11805d059d51bd5e319adfc2b9c
Overthrive <sara.leal.lessa@gmail.com>
2022-03-13 02:06:33 -0300

    First request

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,9 +1,17 @@
 import Pagination from "components/pagination";
 import MovieCard from "components/MovieCard";
+import { BASE_URL } from "utils/requests";
 
 import './style.css';
+import axios from "axios";
 
 function Listing(){
+
+    //Errado:
+    axios.get(`${BASE_URL}/movies?size=12&page=0`).then(response => {
+        console.log(response.data);
+    });
+
     return(
         <>
         <Pagination />
